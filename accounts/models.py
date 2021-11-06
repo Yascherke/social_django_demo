ab4880d3b527630dfc55e703eebf372e33290453
Yuri <yurisergeev564@gmail.com>
2021-11-06 19:26:20 +0200

    add moduls in accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    email = models.EmailField(blank=True, default="")
+    
+    def __str__(self):
+        return self.email
+    
+class User(models.Model):
+    login = models.CharField(blank=True, max_length=12, default="user")
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.login
+    
+
--- social_django_demo/settings.py
+++ social_django_demo/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'social_django_demo',
+
+    'accounts',
+    'chats',
 ]
 
 MIDDLEWARE = [
